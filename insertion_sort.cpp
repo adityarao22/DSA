54ff122812e9333e7b83b03af24c5a25f74c7c6a
blaze1902 <arao3893@gmail.com>
2026-08-13 16:37:20 +0530

    add

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -1,1 +1,21 @@
-#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+void insertion_sort(int arr[],int n){
+
+}
+
+int main(){
+    int i,n;
+    cin>>n;
+    int arr[n];
+    for(i=0;i<n;i++){
+        cout<<arr[i];
+    }
+    
+    insertion_sort(arr,n);
+
+    for(i=0;i<n;i++){
+        cout<<arr[i];
+    }
+}

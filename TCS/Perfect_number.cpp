86b59a3158f80ca58c3429d8b75cfc735771cc3d
blaze1902 <arao3893@gmail.com>
2026-09-03 17:09:22 +0530

    add

---
+++ TCS/Perfect_number.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter a number :";
+    cin>>n;
+    int sum=0;
+    for(int i=1;i<n-1;i++){
+          if(n%i==0){
+            sum +=i;
+          }
+    }
+    if(sum==n){
+        cout<<"Perfect number";
+    }
+    else{
+        cout<<"Not a perfect number";
+    }
+    return 0;
+}
